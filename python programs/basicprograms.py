668fe0425b09bd1c1415140d1b0ae7b75a063304
mamta-dhende <99284286+mamta-dhende@users.noreply.github.com>
2022-02-17 23:59:49 +0530

    Add files via upload

---
+++ python programs/basicprograms.py
@@ -0,0 +1,48 @@
+#Write a program to take input of userid and password from the user and verify it
+#username=input('Enter Username:')
+#password=input('Enter Password:')
+#if username=='neha'and password=='mamta':
+ #    print('Valid User')
+#else:
+ #    print('Invalid User') 
+
+ #Taking input from the user like eno, ename, esal, eaddr, married
+#eno=int(input('Enter Employee Number:'))
+#ename=input('Enter Employee Name:')
+#esal=float(input('Enter Employee Salary:'))
+#eaddr=input('Enter Employee Adress:')
+#married=eval(input('Is employee married ?[True/False]:'))
+#print('Please Confirm Your Provided Information')
+#print('Employee Number:',eno)
+#print('Employee Name:',ename)
+#print('Employee Salary:',esal)
+#print('Employee Address:',eaddr)
+#print('Married:',married)
+
+#End Operator
+#a= 'XYZ Company'
+#b='Software'
+#c='Solutions'
+#print(a,end=' ')
+#print(b,end='**')
+#print(c,end=' @@@@@')
+
+#1:Take input and send a message by greeting them with their percentage record
+#enrollmentno=int(input('Enter Your Enrollment Number :'))
+#if enrollmentno==34:
+ #   print('Heyy Sam, glad to inform you that your percentage is beyond expectations')
+#else:
+#   print('Invalid: Check your Enrollment number again')
+ 
+#Write a program by taking input from the user and informing them with the prices
+#item=(input('Enter your food-item here to know the price :'))
+#if item=='Pav Bhaji':
+#    print('60 Rs')
+#elif item=='Chocolate Ice-cream':
+#    print('40 Rs')
+#elif item=='Misal Pav':
+#    print('80 Rs')    
+#elif item=='Shev Pav':
+#    print('25 Rs')    
+#else:
+#    print('Opps! Currently this item is not available')    
